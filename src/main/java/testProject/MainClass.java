1bd64a056369a895e537784c3a55e7e2774c6cfa
HJin90 <rooriim990@gmail.com>
2020-03-15 16:41:33 +0900

    branch test1

--- src/main/java/testProject/MainClass.java
+++ src/main/java/testProject/MainClass.java
@@ -9,6 +9,7 @@ public class MainClass {
 		// TranspotationWalk transpotationWalk = new TranspotationWalk();
 		// transpotationWalk.move();
 		//testing
+		// showing only for HJ
 		// applicationContext(컨테이너)에 접근
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 		// Bean의 id, dataType
