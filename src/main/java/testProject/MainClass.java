3588830b74e09f6cdc5987d331b2a2255d658048
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2020-03-15 15:05:03 +0900

    Initial commit

---
+++ src/main/java/testProject/MainClass.java
@@ -0,0 +1,23 @@
+package testProject;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TranspotationWalk transpotationWalk = new TranspotationWalk();
+		// transpotationWalk.move();
+		
+		// applicationContext(컨테이너)에 접근
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		// Bean의 id, dataType
+		TranspotationWalk transpotationWalk = ctx.getBean("tWalk", TranspotationWalk.class);
+		transpotationWalk.move();
+		
+		ctx.close();
+	}
+
+}
+
+
